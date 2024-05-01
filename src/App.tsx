ea17a93670a81011b2a3db18ea2c562e369fc370
Kosuke <tirol.developer@gmail.com>
2024-05-01 16:39:50 +0900

    MyDescriptions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Empty } from 'antd';
 import CustomizeTheme from './CustomizeTheme';
 import MyAutoComplete from './MyAutoComplete';
+import MyDescriptions from './MyDescriptions';
 
 const App = () => (
   <div className="App">
     <CustomizeTheme/>
     <MyAutoComplete />
+    <MyDescriptions />
+    <Empty/>
   </div>
 );
 
